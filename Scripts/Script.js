6e8b5c7ce4b0de516ee21308a77194e1c8a1ea6a
Hojjat Hekmatipour <158481246+Hojjat-hk@users.noreply.github.com>
2024-02-05 21:32:45 +0330

    Fixed Script.js
    V,03

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -25,17 +25,13 @@ function bmiCalculator(){
     resultBMINum.innerHTML = bmiResult.toFixed(2);
     
     if(bmiResult >= 0 && bmiResult <= 18.5){
-        resultBMItext.innerHTML = 'underweight';
-        resultBMINum.style.color = "#FFBC00";
+        advanceWriting('underweight', "#FFBC00");
     }else if(bmiResult >= 18.6 && bmiResult <= 24.9){
-        resultBMItext.innerHTML = 'normal';
-        resultBMINum.style.color = "#099C00";
+        advanceWriting('normal', "#099C00");
     }else if(bmiResult >= 25 && bmiResult <= 29.9){
-        resultBMItext.innerHTML = 'Overweight';
-        resultBMINum.style.color = "#E44900";
+        advanceWriting('Overweight', "#E44900");
     }else if(bmiResult >= 30){
-        resultBMItext.innerHTML = 'Obese';
-        resultBMINum.style.color = "#C20600";
+        advanceWriting('Obese', "#C20600");
     }
 }
 function advanceWriting(text = "", bmiColor = "") {
